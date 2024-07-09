bdbdd8a0b2413fe6d8f310e0927e1172653d815d
Marcello Yoel Christianus <marcelloyoell@gmail.com>
2024-07-09 12:15:42 +0700

    test ubah search bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
             <form action="{{ route('update-address') }}" method="post">
                 @csrf
                 <div class="form-group">
-                    <label for="address">Enter Address:</label>
+                    <label for="address">A:</label>
                     <input type="text" class="form-control" id="address" name="address">
                 </div>
                 <button type="submit" class="btn btn-primary">Update Address</button>
@@ -31,24 +31,24 @@
                 $distance = (float) str_replace(' km', '', $calculatedDistances[$laundry->user_id] ?? PHP_INT_MAX);
             @endphp
             @if ($distance <= 25) --}}
-                <div class="col-sm-3">
-                    <div class="card mt-3" style="width: 18rem;">
-                        @if ($laundry->picture)
-                            <img src="{{ asset('images/' . $laundry->picture) }}" class="card-img-top">
-                        @else
-                            <img src="{{ asset('images/shopimagedummy.png') }}" class="card-img-top">
-                        @endif
-                        {{-- <img src={{url('storage/image/shopimagedummy.png')}} class="card-img-top"> --}}
-                        <div class="card-body">
-                            <h5 class="card-title" style="font-weight: bold">{{ $laundry->laundrySepatuName }}</h5>
-                            <h6 class="card-text">📌 : {{ Str::words($laundry->Address, 6, '...') }}</h6>
-                            {{-- <h6 class="card-text">📌 : {{ $laundry->Address }}</h6> --}}
-                            <h6 class="card-text">☎ : {{ $laundry->Contact }}</h6>
-                            <h6 class="card-text">📍 : {{ $calculatedDistances[$laundry->user_id] ?? 'Not available' }} </h6>
-                            <a href="laundry/{{ $laundry->id }}" class="btn btn-primary mt-2">Laundry Detail</a>
-                        </div>
+            <div class="col-sm-3">
+                <div class="card mt-3" style="width: 18rem;">
+                    @if ($laundry->picture)
+                        <img src="{{ asset('images/' . $laundry->picture) }}" class="card-img-top">
+                    @else
+                        <img src="{{ asset('images/shopimagedummy.png') }}" class="card-img-top">
+                    @endif
+                    {{-- <img src={{url('storage/image/shopimagedummy.png')}} class="card-img-top"> --}}
+                    <div class="card-body">
+                        <h5 class="card-title" style="font-weight: bold">{{ $laundry->laundrySepatuName }}</h5>
+                        <h6 class="card-text">📌 : {{ Str::words($laundry->Address, 6, '...') }}</h6>
+                        {{-- <h6 class="card-text">📌 : {{ $laundry->Address }}</h6> --}}
+                        <h6 class="card-text">☎ : {{ $laundry->Contact }}</h6>
+                        <h6 class="card-text">📍 : {{ $calculatedDistances[$laundry->user_id] ?? 'Not available' }} </h6>
+                        <a href="laundry/{{ $laundry->id }}" class="btn btn-primary mt-2">Laundry Detail</a>
                     </div>
                 </div>
+            </div>
             {{-- @endif --}}
         @endforeach
 
@@ -85,8 +85,8 @@
     </div>
     <div>
         <script>
-            @if(!Session::has('locationAlertShown'))
-                @if(session('latitude') === null && session('longitude') === null)
+            @if (!Session::has('locationAlertShown'))
+                @if (session('latitude') === null && session('longitude') === null)
                     alert('We can\'t find your location, please input manually!');
                 @endif
                 {{ Session::put('locationAlertShown', true) }}
